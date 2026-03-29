fab9a7029ca95873d61adc0daacb67bab52412f3
Angel Macwan <armacwan@gmail.com>
2026-03-29 14:06:03 +0530

    fet

--- _fix_metrics.py
+++
@@ -1,5 +0,0 @@
-p = '/Users/angel/Documents/Thallus/api/routers/metrics.py'
-lines = open(p).readlines()
-with open(p, 'w') as f:
-    f.writelines(lines[:179])
-print(f"Done. Wrote {len(lines[:179])} lines.")
